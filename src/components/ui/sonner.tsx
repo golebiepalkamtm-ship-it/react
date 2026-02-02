3bfbe44497e173e4831092d7d41d20e611d44cad
Marcin Pałka <golebie.palka.mtm@gmail.com>
2026-02-02 16:05:20 +0100

    fix(ui): increase toaster z-index to 99999

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,7 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group !z-[99999]"
       position="center"
       toastOptions={{
         classNames: {
