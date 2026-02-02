79c126200d4771b4008ffa13c2dfcd665224ef1c
Marcin Pałka <golebie.palka.mtm@gmail.com>
2026-02-02 16:15:45 +0100

    fix(ui): boost z-index for both Sonner and standard Toaster to 99999

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-4 left-1/2 transform -translate-x-1/2 z-[100] flex max-h-screen w-full max-w-md flex-col-reverse p-4",
+      "fixed top-4 left-1/2 transform -translate-x-1/2 !z-[99999] flex max-h-screen w-full max-w-md flex-col-reverse p-4",
       className,
     )}
     {...props}
