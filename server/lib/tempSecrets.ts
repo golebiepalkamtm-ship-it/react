79dded3c508553affc4994397c3e67ae809f8e6f
Marcin Pałka <golebie.palka.mtm@gmail.com>
2026-02-10 15:08:32 +0100

    Initialize production secrets fallback

---
+++ server/lib/tempSecrets.ts
@@ -0,0 +1,3 @@
+export const SESSION_SECRET = 'paka_mtm_premium_session_2026_safe_3bd9a8c2f1';
+export const SUPABASE_SECRET_ACCESS_KEY = '2444c764fef478b9092a4a7ca5ec0387';
+export const SUPABASE_SECRET_SECRET_KEY = '7b4927c43f0a8611e24ac7b21c513bafdae23cad9e0be242cc95c9d2eaa0e99e';
