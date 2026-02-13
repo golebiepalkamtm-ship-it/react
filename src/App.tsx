cb503105b1a42b4880716a4fd0e1db496d2347ac
Marcin Pałka <golebie.palka.mtm@gmail.com>
2026-02-13 13:23:26 +0100

    style: slightly darken global background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ const BackgroundWrapper = () => {
           right: 0,
           bottom: 0,
           zIndex: -100,
-          background: 'linear-gradient(to bottom, #A8A19F 0%, #F4F3E3 100%)',
+          background: 'linear-gradient(to bottom, #867F7D 0%, #D6D5C2 100%)',
           pointerEvents: 'none'
         }}
       />
